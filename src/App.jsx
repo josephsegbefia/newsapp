457a1390f467e1841893399fc40cf6651d83428a
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-16 00:45:01 +0200

    Add temp list to hold stories

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,33 @@ const welcome = {
   title: "React"
 };
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+  {
+    title: "Express",
+    url: "https://expressjs.com/",
+    author: "TJ Holowaychuk",
+    num_comments: 3,
+    points: 4,
+    objectID: 2
+  }
+];
+
 function App() {
   return (
     <div>
@@ -16,6 +43,21 @@ function App() {
       </h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
