c8d7d41023a4c7ca38cbad87e843eb2a925705c8
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-16 00:01:03 +0200

    Add rudimentary search bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const welcome = {
+  greeting: "Hey",
+  title: "React"
+};
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
     </div>
   );
 }
